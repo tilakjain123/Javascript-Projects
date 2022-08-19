1dcd492b94c7089342b1c14eaa255f1ea5425af9
Tilak Jain <89719256+tilakjain123@users.noreply.github.com>
2022-08-19 16:05:19 +0530

    Create script.js

---
+++ Slideshow/script.js
@@ -0,0 +1,21 @@
+var slide_index = 1;  
+        displaySlides(slide_index);  
+  
+        function nextSlide(n) {  
+            displaySlides(slide_index += n);  
+        }  
+  
+        function currentSlide(n) {  
+            displaySlides(slide_index = n);  
+        }  
+  
+        function displaySlides(n) {  
+            var i;  
+            var slides = document.getElementsByClassName("showSlide");  
+            if (n > slides.length) { slide_index = 1 }  
+            if (n < 1) { slide_index = slides.length }  
+            for (i = 0; i < slides.length; i++) {  
+                slides[i].style.display = "none";  
+            }  
+            slides[slide_index - 1].style.display = "block";  
+        }  
