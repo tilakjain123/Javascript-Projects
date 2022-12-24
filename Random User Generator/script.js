f5450d14acb7cca2fb9d94dc647d2a895e9a03db
REZA <effati78@protonmail.com>
2022-12-24 22:38:31 +0330

    random user generator reviewed

--- Random User Generator/script.js
+++ Random User Generator/script.js
@@ -4,19 +4,23 @@ let getUserBtn = document.getElementById("get-user-btn");
 let url = "https://random-data-api.com/api/v2/users?response_type=json";
 
 let getUser = () => {
-  fetch(url)
-    .then((resp) => resp.json())
-    .then((data) => {
-      imgContainer.innerHTML = `<img src= ${data.avatar}>`;
-      details.innerHTML = `
+    fetch(url)
+        .then((resp) => resp.json())
+        .then((data) => {
+            imgContainer.innerHTML = `<img src= ${data.avatar}>`;
+            details.innerHTML = `
       <h2>${data.first_name} ${data.last_name}</h2>
       <h3>${data.employment.title}</h3>
       <h4><i class="fa-solid fa-location-dot"></i> ${data.address.city}</h4>`;
 
-      let randomColor =
-        "#" + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, "0");
-      document.documentElement.style.setProperty("--theme-color", randomColor);
-    });
+            let randomColor =
+                "#" +
+                ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, "0");
+            document.documentElement.style.setProperty(
+                "--theme-color",
+                randomColor
+            );
+        });
 };
 window.addEventListener("load", getUser);
 getUserBtn.addEventListener("click", getUser);
