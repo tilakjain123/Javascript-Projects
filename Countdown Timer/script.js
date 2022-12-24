c7ff667dc2fc50b627d33e055e059d9e0655dd82
REZA <effati78@protonmail.com>
2022-12-24 22:35:52 +0330

    countdown timer reviewed

--- Countdown Timer/script.js
+++ Countdown Timer/script.js
@@ -1,19 +1,20 @@
 let demo = document.getElementById("demo");
-let deadline = new Date("Nov, 2022, 12:37:25").getTime(); 
-let x = setInterval(function() { 
-let now = new Date().getTime(); 
-let time = deadline - now; 
-let days = Math.floor(time / (1000 * 60 * 60 * 24)); 
-var hours = Math.floor((time%(1000 * 60 * 60 * 24))/(1000 * 60 * 60)); 
-var minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)); 
-var seconds = Math.floor((time % (1000 * 60)) / 1000); 
-demo.innerHTML = days + "d "  
-+ hours + "h " + minutes + "m " + seconds + "s ";
-conhahsole.log(days + "d "  
-+ hours + "h " + minutes + "m " + seconds + "s ");
-// check the time if its expired
-    if (time < 0) { 
-        clearInterval(x); 
-        demo.innerHTML = "Time is Up!"; 
-    } 
-}, 1000); 
+let deadline = new Date("Nov, 2022, 12:37:25").getTime();
+let x = setInterval(function () {
+    let now = new Date().getTime();
+    let time = deadline - now;
+    let days = Math.floor(time / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((time % (1000 * 60)) / 1000);
+    demo.innerHTML =
+        days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    conhahsole.log(
+        days + "d " + hours + "h " + minutes + "m " + seconds + "s "
+    );
+    // check the time if its expired
+    if (time < 0) {
+        clearInterval(x);
+        demo.innerHTML = "Time is Up!";
+    }
+}, 1000);
