0856334424a8f87ced74207b4004271ef1042669
REZA <effati78@protonmail.com>
2022-12-24 22:35:22 +0330

    calculator reviewed

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,25 +1,20 @@
 // display function
-function display(val){
+function display(val) {
+    document.getElementById("result").value += val;
 
-    document.getElementById('result').value += val
-
-    return val
-
+    return val;
 }
 // Main logic
-function solve(){
-
-    let x = document.getElementById('result').value
+function solve() {
+    let x = document.getElementById("result").value;
 
     let y = eval(x);
 
-    document.getElementById('result').value = y
+    document.getElementById("result").value = y;
 
-    return y
+    return y;
 }
 // Erase the screen
-function clearScreen(){
-
-    document.getElementById('result').value = ''
-
+function clearScreen() {
+    document.getElementById("result").value = "";
 }
